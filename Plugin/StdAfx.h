02084eacfa8d356edc2f9ec453db0fdd7b883520
Angel <121910047+KazukiGames82@users.noreply.github.com>
2024-12-15 13:44:31 -0600

    1.1.9.0

---
+++ Plugin/StdAfx.h
@@ -0,0 +1,21 @@
+
+#pragma once
+#pragma comment(lib, "dwmapi.lib")
+
+// WinAPI
+#include <windows.h>
+
+#include <dwmapi.h>
+#include <VersionHelpers.h>
+
+// STL
+#include <stdexcept>
+#include <string>
+
+// Rainmeter API
+#include "../API/RainmeterAPI.h"
+
+#define UNUSED(expr)  \
+    do {              \
+        (void)(expr); \
+    } while (0)
